c32e5845ec25759738c275a5e56f8307e22246d8
EMILKDE <razer.saiso@gmail.com>
2020-04-23 21:03:29 +0200

    added user input for pwd tried to compared string but still working on it.

--- bash-learner.c
+++ bash-learner.c
@@ -1,12 +1,28 @@
 /* Author: RazerSAISO */
 /* This program will guide you through learning the command line interface (CLI) mainly the bash shell */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define MAX_LIMIT 20
+
 int main()
 {
-   char str[MAX_LIMIT];	
-   printf("Hello and Welcome to bash learner, I am you're bash teacher my name is Basher.\n");
+   char str[MAX_LIMIT];
+   char cmd[100];   
+   int result;
+   printf("Hello and Welcome to bash learner, I am you're bash teacher my name is Basher. What's you're name ?\n");
    fgets(str, MAX_LIMIT, stdin); 
    printf("Hi %s", str); 
+   printf("In the shell you can move between 'folders' called directories the first command you will learn is pwd it shows in which directorie you are in. Try it write pwd! \n ");
+   fgets(str, MAX_LIMIT, stdin);
+   result = strcmp(str,"pwd");
+   sprintf(cmd, "%s\n", str);
+   system(cmd);
+   if(result == 0)
+   {
+        sprintf(cmd, "%s\n", str);
+	system(cmd);
+	printf("Good Job you just learned your first command.");
+   }	    
    return 0;
 }
--- gitadd+commit.sh
+++ gitadd+commit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #THIS FILE IS MADE TO ADD FILES COMMIT AND PUSH AT THE SAME TIME.
 echo "ADDING FILES TO YOUR REPOSITORY"
-git add *
+git add * # add files to repo 
 echo "What is you're commit message ?"
-read msg 
-git commit -m "$msg"
+read msg # read message for commit
+git commit -m "$msg" #publish commit
 echo "Pushing to your repository"
-git push origin master
+git push origin master #push everything
