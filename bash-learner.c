dc6c5a777b7b34396704506bdec2f071fb9ec5a0
Razer <razer.saiso@gmail.com>
2020-05-04 23:50:09 +0200

    added the cd command and comparison of substrings so the cd compare only took one substring that is the cd part of the command.

--- bash-learner.c
+++ bash-learner.c
@@ -7,32 +7,31 @@
 
 int main()
 {
-   char str[MAX_LIMIT];
-   char cmd[100];   
+   char st[MAX_LIMIT];
+   char cmd[100];
+   char compar[] = "cd"; //This is the string that we will that we will compare later.   
    int result;
    printf("Hello and Welcome to bash learner, I am you're bash teacher my name is Basher. What's you're name ?\n");
-   fgets(str, MAX_LIMIT, stdin); 
-   printf("Hi %s", str); 
+   fgets(st, MAX_LIMIT, stdin); 
+   printf("Hi %s", st); 
    printf("In the shell you can move between 'folders' called directories the first command you will learn is pwd it shows in which directory you are in. Try it write pwd! \n ");
-   fgets(str, MAX_LIMIT, stdin);
-   result = strcmp(str,"pwd");
+   fgets(st, MAX_LIMIT, stdin);
+   result = strcmp(st,"pwd");
    if(result == 10)
    {
-        sprintf(cmd, "%s\n", str);
+        sprintf(cmd, "%s\n", st);
 	system(cmd);
 	printf("Good Job you just learned your first command!\n");
         printf("Now that you've learned how to see in what directory you now need to know how to move from one directory to another. For that you need to know the command cd which stands for Change Directory and add the name of the directory. It should look like this: cd nameofdirectory. Try it!: \n");
-        fgets(str, MAX_LIMIT, stdin);
-        result = strcmp(str,"cd");
-	printf("%d", result);
-        if(result == 10)
-	{
-	    sprintf(cmd, "%s\n", str);
-            system(cmd);
-	    printf("Good Job you just learned how to move from another directory to another.");
-
-	}
-
+        fgets(st, MAX_LIMIT, stdin);
+        if (strstr(st, compar) == NULL)
+        {
+            printf("Wrong retry. cd + the name of the directory.");
+        }
+        else
+        {
+            printf("Good Job you just learned how to move from another directory to another.");
+        }
    }
    return 0;
 }
