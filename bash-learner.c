39390c5a7b4e23fdb939932a54a8766f73a8e23b
EMILKDE <razer.saiso@gmail.com>
2020-05-16 12:34:44 +0200

    Added checkpoints to restart were the user left off almost done have to find how to read and check string in file.

--- bash-learner.c
+++ bash-learner.c
@@ -4,16 +4,28 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX_LIMIT 20
+#define MAXCHAR 1000
 
 int main()
 {
    char st[MAX_LIMIT];
    char cmd[100];
    char compar[] = "cd"; //This is the string that we will that we will compare later.   
+   char rf[MAXCHAR];
+   char ch;
    int result;
+   FILE *fp;
+
+   fp = fopen("place.txt", "w+");//open place.txt
+   fprintf(fp,"pwd\n");//write 
+   if(fp == NULL)
+   {
+      printf("Error! Could not create a text file. Please report this error on GitHub");   
+      exit(1);             
+   }
    printf("Hello and Welcome to bash learner, I am you're bash teacher my name is Basher. What's you're name ?\n");
    fgets(st, MAX_LIMIT, stdin); 
-   printf("Hi %s", st); 
+   printf("Hi %s", st);
    printf("In the shell you can move between 'folders' called directories the first command you will learn is pwd it shows in which directory you are in. Try it write pwd! \n ");
    fgets(st, MAX_LIMIT, stdin);
    result = strcmp(st,"pwd");
@@ -21,6 +33,7 @@ int main()
    {
         sprintf(cmd, "%s\n", st);
 	system(cmd);
+	fprintf(fp,"cd\n");
 	printf("Good Job you just learned your first command!\n");
         printf("Now that you've learned how to see in what directory you now need to know how to move from one directory to another. For that you need to know the command cd which stands for Change Directory and add the name of the directory. It should look like this: cd nameofdirectory. Try it!: \n");
         fgets(st, MAX_LIMIT, stdin);
@@ -30,6 +43,7 @@ int main()
         }
         else
         {
+	    fprintf(fp,"ls\n");
             printf("Good Job you just learned how to move from another directory to another. \n");
 	    printf("Now you may think : Basher how do I see the possible directories in which I can go in ? \n ");
 	    printf("It's good you thought of that to look at the directories you can go in, you need the following command: ls. It stands for list. Try it! :\n ");
@@ -38,10 +52,11 @@ int main()
             if (result == 10)
             {
 	    	printf("\n Good Job ! You know how look at the directories! \n");
-		//test
+		
 	    }
         }
 
    }
+
    return 0;
 }
