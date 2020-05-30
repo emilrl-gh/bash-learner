84785f00e990f3c4a0ee74ceb771f7ed5cd47744
Razer <razer.saiso@gmail.com>
2020-05-30 15:53:50 +0200

    Added fix for checkpoints so you could start back from pwd and cd. I will add ls later.

--- bash-learner.c
+++ bash-learner.c
@@ -7,43 +7,80 @@
 #define MAX_LIMIT 20
 #define MAXCHAR 1000
 
-int compare_strings(char [], char []); 
-
-int compare_strings(char a[], char b[])
-{
-   int c = 0;
- 
-   while (a[c] == b[c]) {
-      if (a[c] == '\0' || b[c] == '\0')
-         break;
-      c++;
-   }
-   
-   if (a[c] == '\0' && b[c] == '\0')
-      return 0;
-   else
-      return -1;
-}
-
-
 int main()
 {
-   char* var[];
    char st[MAX_LIMIT];
    char cmd[100];
-   char compar[20] = "cd" ; //This is the string that we will that we will compare later.   
-   char comparls[20];
-   char* comparpwd[20];
+   char compar[20] = "cd" ; //This is the string that we will that we will compare later.
    char rf[MAXCHAR];
    char ch;
    int result;
-   int results;
+   char str[MAXCHAR];
+   char* filename = "place.txt";
+   const char* findpwd = "pwd";
+   const char* findcd = "cd";
+   const char* findls = "ls";
+   filename = "place.txt";
    FILE *fp;
-   printf("Checking place.txt for a checkpoint."); 
-   fp = fopen("place.txt", "r+");//open place.txt
-   while ((var=fgetc(fp)) != EOF)
-   printf("%c", var);
-   comparpwd = "pwd";
+   
+   fp = fopen(filename, "r+");
+   if (fp == NULL){
+       printf("Could not open file %s",filename);
+       return 1;
+   }
+   while (fgets(str, MAXCHAR, fp) != NULL)
+       printf("%s", str);
+   if (strcmp (str, findpwd) == 10) {
+       printf("Found checkpoint !\n");
+       printf("In the shell you can move between 'folders' called directories the first command you will learn is pwd it shows in which directory or folder you are in. you can name directories whatever you want. Try it write pwd! \n ");
+       fgets(st, MAX_LIMIT, stdin);
+       result = strcmp(st,"pwd");
+       if(result == 10)
+       {
+            sprintf(cmd, "%s\n", st);
+        system(cmd);
+        fprintf(fp,"cd\n");
+        printf("Good Job you just learned your first command!\n");
+            printf("Now that you've learned how to see in what directory you now need to know how to move from one directory to another. For that you need to know the command cd which stands for Change Directory and add the name of the directory. It should look like this: cd nameofdirectory. Try it!: \n");
+            fgets(st, MAX_LIMIT, stdin);
+            if (strstr(st, compar) == NULL)
+            {
+                printf("Wrong retry. cd + the name of the directory.");
+            }
+            else
+            {
+            fprintf(fp,"ls\n");
+            sprintf(cmd, "%s\n", st);
+                system(cmd);
+                printf("Good Job you just learned how to move from another directory to another. To move out of a directory you use this specific command: cd .. \n");
+            printf("Now you may think : Basher how do I see the possible directories in which I can go in ? \n ");
+            printf("It's good you thought of that to look at the directories you can go in, you need the following command: ls. It stands for list. Directories that start with . while not be shown Try it! :\n ");
+            fgets(st,MAX_LIMIT, stdin);
+            result = strcmp(st,"ls");
+                if (result == 10)
+                {
+            sprintf(cmd, "%s\n", st);
+                    system(cmd);
+                printf("\n Good Job ! You know how look at the directories! \n");
+            printf("\n Something I did not tell you is that almost each command has what is called a 'parameter' by that I mean an 'argument' you can add a parameter so you can see what you really want to see for example in the ls command you can add -a to see every directories even the ones that start with . ");
+            fgets(st,MAX_LIMIT, stdin);
+                    result = strcmp(st,"ls -a");
+            if (result == 10)
+            {
+                        sprintf(cmd, "%s\n", st);
+                        system(cmd);//run command from user input
+            }
+
+            
+            }
+            }
+       }
+       return 0;
+   }
+   else if (strcmp (str, findcd) == 10)
+   {
+       printf("Found cd checkpoint! \n");
+   }
    fprintf(fp,"pwd\n");//write
    
    if(fp == NULL)
@@ -102,3 +139,4 @@ int main()
 
    return 0;
 }
+
