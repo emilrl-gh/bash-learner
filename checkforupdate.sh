a75c40fbe34aa0506ab28043ae93938ffb0908f1
Razer <razer.saiso@gmail.com>
2020-07-27 00:10:04 +0200

    repaired problem with checking for updates

--- checkforupdate.sh
+++ checkforupdate.sh
@@ -8,11 +8,5 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 bold=`tput bold`
-if [ $LOCAL = $REMOTE ]; then
-    echo "${green} ${bold}"
-    echo "Bash-learner is up-to-date ${reset}"
-elif [ $LOCAL = $BASE ]; then
-    echo "${green}Need to update using the command: make update. Please type Control + C to stop and run the command.${reset}"
-else
-    echo ""
-fi
+[ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
+sed 's/\// /g') | cut -f1) ] && echo "${bold}${green}Bash-learner is up-to-date ${reset}" || echo "${bold}${red}Need to update using the command: make update. Please type Control + C to stop and run the command.${reset}"
