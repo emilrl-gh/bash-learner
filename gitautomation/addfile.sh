ad32fb96fbfd3266d1ebb893049366dde188af52
Razer <razer.saiso@gmail.com>
2020-08-05 23:24:12 +0200

    Removed newly ignored gitautomation

--- gitautomation/addfile.sh
+++
@@ -1,9 +0,0 @@
-#This file is for commiting pushing and pulling one specific file. Example use: addfile.sh nameoffile
-git add $1
-echo "What is your commit message ?"
-read msg
-git commit -m "$msg"
-echo "Pulling"
-git pull
-echo "Pushing to your repository"
-git push origin master
