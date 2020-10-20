8191b2ebd5a3dd54dfd1606f29c0e3af562a1e55
K0stad1n <razer.saiso@gmail.com>
2020-10-20 23:10:59 +0200

    Added ascii art, squashed some bugs, changed version number

--- src/bash-learner.c
+++ src/bash-learner.c
@@ -13,9 +13,17 @@
 #define MAXL 2000
 int main()
 {
+   char st[200];//This string is used for getting user input
    char* filename = "place.txt"; // file for storing checkpoint
    FILE *fp; //This var is for opening place.txt
    fp = fopen(filename, "r+");
+   ascii_art();
+   if (fp == NULL){
+       fprintf(stderr, "%s: %s!\n", filename, strerror(errno));
+       printf("Created %s file\n", filename);
+       system("touch place.txt");
+       fp = fopen(filename, "r+");
+   }
    system("git pull");
    readcheckpoint(fp, filename);
    alltext();
--- src/bashltext.c
+++ src/bashltext.c
@@ -3,13 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include "colors.h"
-#define MAXL 2000
 #define MAXCHAR 2000
+
+void ascii_art(){
+  printf("\n");
+  printf("██████╗  █████╗ ███████╗██╗  ██╗      ██╗     ███████╗ █████╗ ██████╗ ███╗   ██╗███████╗██████╗\n");
+  printf("██╔══██╗██╔══██╗██╔════╝██║  ██║      ██║     ██╔════╝██╔══██╗██╔══██╗████╗  ██║██╔════╝██╔══██╗\n");
+  printf("██████╔╝███████║███████╗███████║█████╗██║     █████╗  ███████║██████╔╝██╔██╗ ██║█████╗  ██████╔╝\n");
+  printf("██╔══██╗██╔══██║╚════██║██╔══██║╚════╝██║     ██╔══╝  ██╔══██║██╔══██╗██║╚██╗██║██╔══╝  ██╔══██╗\n");
+  printf("██████╔╝██║  ██║███████║██║  ██║      ███████╗███████╗██║  ██║██║  ██║██║ ╚████║███████╗██║  ██║\n");
+  printf("╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝      ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝\n");
+  printf("\n");
+}
 
 void pwd_text(){
   char st[200];//This string is used for getting user input
   char cmd[100]; //This string is used to run actual commands in the shell.
-  int result; //this interger is used as the output of comparing two strings.
+  int result; //this integer is used as the output of comparing two strings.
   int result2;
   char str[200]; //str is used as a string to store the checkpoints read in place.txt
   char* filename = "place.txt"; // file for storing checkpoint
@@ -43,7 +53,7 @@ void pwd_text(){
 void cd_text(){
   char st[200];//This string is used for getting user input
   char cmd[100]; //This string is used to run actual commands in the shell.
-  int result1; //this interger is used as the output of comparing two strings.
+  int result1; //this integer is used as the output of comparing two strings.
   int result2;
   char str[200]; //str is used as a string to store the checkpoints read in place.txt
   printf(FONT_BOLD BRIGHT_BLUE "%s", "To go into a directory you have to know the cd command. You use it as follows: cd + nameofdirectory. Try it!(type cd test): \n" ANSI_RESET);
@@ -72,7 +82,7 @@ void cd_text(){
 void ls_text(){
   char st[200];//This string is used for getting user input
   char cmd[100]; //This string is used to run actual commands in the shell.
-  int result; //this interger is used as the output of comparing two strings.
+  int result; //this integer is used as the output of comparing two strings.
   int result2;
   char str[200]; //str is used as a string to store the checkpoints read in place.txt
   char* filename = "place.txt"; // file for storing checkpoint
@@ -106,7 +116,7 @@ void ls_text(){
 void ls_atext(){
   char st[200];//This string is used for getting user input
   char cmd[100]; //This string is used to run actual commands in the shell.
-  int result; //this interger is used as the output of comparing two strings.
+  int result; //this integer is used as the output of comparing two strings.
   int result2;
   char str[200]; //str is used as a string to store the checkpoints read in place.txt
   printf(FONT_BOLD COLOR_YELLOW "%s%s", "Each command has a 'parameter'. Each parameter starts with - for example command + -a -h For example to see everything",
@@ -135,7 +145,7 @@ void ls_atext(){
 void man_text(){
   char st[200];//This string is used for getting user input
   char cmd[100]; //This string is used to run actual commands in the shell.
-  int result; //this interger is used as the output of comparing two strings.
+  int result; //this integer is used as the output of comparing two strings.
   int result2;
   char str[200]; //str is used as a string to store the checkpoints read in place.txt
   printf(FONT_BOLD COLOR_YELLOW "%s%s", "The man command is used to know everything about a command. ",
@@ -164,7 +174,7 @@ void man_text(){
 void mkdir_text(){
   char st[200];//This string is used for getting user input
   char cmd[100]; //This string is used to run actual commands in the shell.
-  int result; //this interger is used as the output of comparing two strings.
+  int result; //this integer is used as the output of comparing two strings.
   int result2;
   char str[200]; //str is used as a string to store the checkpoints read in place.txt
   printf(FONT_BOLD COLOR_BLUE "%s%s", "To create a directory you need to know the command mkdir which stands for make directory. ",
@@ -193,7 +203,7 @@ void mkdir_text(){
 void rmdir_text(){
   char st[200];//This string is used for getting user input
   char cmd[100]; //This string is used to run actual commands in the shell.
-  int result; //this interger is used as the output of comparing two strings.
+  int result; //this integer is used as the output of comparing two strings.
   int result2;
   char str[200]; //str is used as a string to store the checkpoints read in place.txt
   printf(FONT_BOLD COLOR_BLUE "%s%s", "To remove a directory you need to know the command rmdir which stands for remove directory. ",
@@ -222,7 +232,7 @@ void rmdir_text(){
 void touchtext(){
   char st[200];//This string is used for getting user input
   char cmd[100]; //This string is used to run actual commands in the shell.
-  int result; //this interger is used as the output of comparing two strings.
+  int result; //this integer is used as the output of comparing two strings.
   int result2;
   char str[200]; //str is used as a string to store the checkpoints read in place.txt
   printf(FONT_BOLD COLOR_YELLOW "%s", "To create a file you have to know the touch command. You use it like this touch nameoffile. Try it (type touch testfile)!: \n" ANSI_RESET);
@@ -251,7 +261,7 @@ void touchtext(){
 void rmtext(){
   char st[200];//This string is used for getting user input
   char cmd[100]; //This string is used to run actual commands in the shell.
-  int result; //this interger is used as the output of comparing two strings.
+  int result; //this integer is used as the output of comparing two strings.
   int result2;
   char str[200]; //str is used as a string to store the checkpoints read in place.txt
   printf(FONT_BOLD COLOR_YELLOW "%s", "To remove a file you have to know the command rm. To remove a file type rm nameoffile. Try it (type rm testfile)!: \n" ANSI_RESET);
@@ -279,7 +289,7 @@ void rmtext(){
 void texteditor(){
   char st[200];//This string is used for getting user input
   char cmd[100]; //This string is used to run actual commands in the shell.
-  int result; //this interger is used as the output of comparing two strings.
+  int result; //this integer is used as the output of comparing two strings.
   int result2;
   char str[200]; //str is used as a string to store the checkpoints read in place.txt
   printf(FONT_BOLD COLOR_YELLOW "%s", "To edit files there are many text editors. One of them is nano. Try it (type nano nanotext)!: \n" ANSI_RESET);
@@ -311,21 +321,6 @@ void alltext(){
   const char* checkforupdate = "sh checkforupdate.sh"; // check for updates using a shell script
   FILE *fp; //This var is for opening place.txt
   fp = fopen(filename, "r+");
-
-  if (fp == NULL){
-      fprintf(stderr, "%s: %s!\n", filename, strerror(errno));
-      fprintf(stdout, "%s%s%s [%sY%s/%sn%s]> ", FONT_BOLD COLOR_YELLOW, "Continue ? or stop so I can create file.", ANSI_RESET, BRIGHT_GREEN, ANSI_RESET, COLOR_RED, ANSI_RESET);
-      fgets(st, 200, stdin);
-      if (strcmp(st,"Y") == 10){
-        printf("Ok\n");
-      } else {
-        printf("%s%s%s", "Creating file please restart the program.",
-                   " By typing the command bash-learner when you are in",
-                   " linux, on mac type ./bash-learner, on windows rerun the .exe file.\n");
-       system("touch place.txt");
-      }
-  }
-
   printf(FONT_BOLD BRIGHT_GREEN "Hello and Welcome to bash learner,  What's your name ?\n");
   fgets(st, 200, stdin);
   printf("Hi %s I am your bash teacher my name is Basher.", st);
